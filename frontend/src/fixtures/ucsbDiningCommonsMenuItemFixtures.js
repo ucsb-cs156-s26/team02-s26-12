a625baf49eb062c1582e371f649dd3e1965e4f99
Jay Yeung <jayyeung@ucsb.edu>
2026-04-30 17:29:21 -0700

    added fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneItem: {
+    id: 1,
+    diningCommonsCode: "carrillo",
+    name: "Baked Ziti",
+    station: "Entrees",
+  },
+  threeItems: [
+    {
+      id: 1,
+      diningCommonsCode: "carrillo",
+      name: "Baked Ziti",
+      station: "Entrees",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "dlg",
+      name: "Caesar Salad",
+      station: "Salad Bar",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "ortega",
+      name: "Chocolate Chip Cookie",
+      station: "Desserts",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
