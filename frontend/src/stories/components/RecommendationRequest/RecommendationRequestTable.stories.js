c71d36babd21f761ca8cffd185d0c931c455e58b
Pengyu Chen <pengyu_chen@ucsb.edu>
2026-05-03 21:39:45 -0700

    add RecommendationRequest table component

---
+++ frontend/src/stories/components/RecommendationRequest/RecommendationRequestTable.stories.js
@@ -0,0 +1,25 @@
+import React from "react";
+import RecommendationRequestTable from "main/components/RecommendationRequest/RecommendationRequestTable";
+import { recommendationRequestFixtures } from "main/fixtures/recommendationRequestFixtures";
+
+export default {
+  title: "components/RecommendationRequest/RecommendationRequestTable",
+  component: RecommendationRequestTable,
+};
+
+const Template = (args) => {
+  return React.createElement(RecommendationRequestTable, args);
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+  recommendationRequests: [],
+};
+
+export const ThreeItems = Template.bind({});
+
+ThreeItems.args = {
+  recommendationRequests:
+    recommendationRequestFixtures.threeRecommendationRequests,
+};
