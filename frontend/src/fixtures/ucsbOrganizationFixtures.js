87941872093bbc2d69c4903baec0e0373b3bfee1
Pau Marsa <paumarsa@ucsb.edu>
2026-05-03 20:05:14 -0700

    run prettier to fix formatting for fixtures

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,30 +1,30 @@
 const ucsbOrganizationFixtures = {
-    oneOrganization: {
-        orgCode: "OSLI",
-        orgTranslationShort: "Student Life",
-        orgTranslation: "Office of Student Life",
-        inactive: false
+  oneOrganization: {
+    orgCode: "OSLI",
+    orgTranslationShort: "Student Life",
+    orgTranslation: "Office of Student Life",
+    inactive: false,
+  },
+  threeOrganizations: [
+    {
+      orgCode: "NSU",
+      orgTranslationShort: "Nikkei Student Union",
+      orgTranslation: "Nikkei Student Union at UCSB",
+      inactive: false,
     },
-    threeOrganizations: [
-        {
-            orgCode: "NSU",
-            orgTranslationShort: "Nikkei Student Union",
-            orgTranslation: "Nikkei Student Union at UCSB",
-            inactive: false
-        },
-        {
-            orgCode: "TT",
-            orgTranslationShort: "Table Tennis",
-            orgTranslation: "Table Tennis Club at UCSB",
-            inactive: false
-        },
-        {
-            orgCode: "KRC",
-            orgTranslationShort: "Korean Radio",
-            orgTranslation: "Korean Radio Club",
-            inactive: true
-        }
-    ]
+    {
+      orgCode: "TT",
+      orgTranslationShort: "Table Tennis",
+      orgTranslation: "Table Tennis Club at UCSB",
+      inactive: false,
+    },
+    {
+      orgCode: "KRC",
+      orgTranslationShort: "Korean Radio",
+      orgTranslation: "Korean Radio Club",
+      inactive: true,
+    },
+  ],
 };
 
 export { ucsbOrganizationFixtures };
