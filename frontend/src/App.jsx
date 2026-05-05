a7fbc847510ebc535b54a4f070513cba80a92095
Arjun Shah <arjun_shah@ucsb.edu>
2026-05-04 18:17:15 -0700

    Add Articles placeholder pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,10 @@ import MenuItemReviewIndexPage from "main/pages/MenuItemReview/MenuItemReviewInd
 import MenuItemReviewCreatePage from "main/pages/MenuItemReview/MenuItemReviewCreatePage";
 import MenuItemReviewEditPage from "main/pages/MenuItemReview/MenuItemReviewEditPage";
 
+import ArticlesIndexPage from "main/pages/Articles/ArticlesIndexPage";
+import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
+import ArticlesEditPage from "main/pages/Articles/ArticlesEditPage";
+
 import { hasRole, useCurrentUser } from "main/utils/useCurrentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -114,6 +118,28 @@ function App() {
           />
         </>
       )}
+
+      {hasRole(currentUser, "ROLE_USER") && (
+  <>
+    <Route exact path="/articles" element={<ArticlesIndexPage />} />
+  </>
+)}
+{hasRole(currentUser, "ROLE_ADMIN") && (
+  <>
+    <Route
+      exact
+      path="/articles/edit/:id"
+      element={<ArticlesEditPage />}
+    />
+    <Route
+      exact
+      path="/articles/create"
+      element={<ArticlesCreatePage />}
+    />
+  </>
+)}
+
+
     </Routes>
   );
 }
