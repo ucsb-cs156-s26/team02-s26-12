1cd4a8575ead534351bdcb280456429dc0baae5a
Pau Marsa <paumarsa@ucsb.edu>
2026-05-05 19:52:15 -0700

    pm - format error fixed again

--- frontend/src/stories/pages/UCSBOrganization/UCSBOrganizationCreatePage.stories.jsx
+++ frontend/src/stories/pages/UCSBOrganization/UCSBOrganizationCreatePage.stories.jsx
@@ -4,30 +4,33 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { http, HttpResponse } from "msw";
 
 export default {
-    title: 'pages/UCSBOrganization/UCSBOrganizationCreatePage',
-    component: UCSBOrganizationCreatePage
+  title: "pages/UCSBOrganization/UCSBOrganizationCreatePage",
+  component: UCSBOrganizationCreatePage,
 };
 
 const Template = () => <UCSBOrganizationCreatePage storybook={true} />;
 
 export const Default = Template.bind({});
 Default.parameters = {
-    msw: [
-        http.get('/api/currentUser', () => {
-            return HttpResponse.json(apiCurrentUserFixtures.userOnly, {
-                status: 200,
-            });
-        }),
-        http.get('/api/systemInfo', () => {
-            return HttpResponse.json(systemInfoFixtures.showingNeither, {
-                status: 200,
-            });
-        }),
-        http.post('/api/ucsborganization/post', () => {
-            window.alert("POST: /api/ucsborganization/post");
-            return HttpResponse.json({}, {
-                status: 200,
-            });
-        }),
-    ]
-}
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.userOnly, {
+        status: 200,
+      });
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither, {
+        status: 200,
+      });
+    }),
+    http.post("/api/ucsborganization/post", () => {
+      window.alert("POST: /api/ucsborganization/post");
+      return HttpResponse.json(
+        {},
+        {
+          status: 200,
+        },
+      );
+    }),
+  ],
+};
