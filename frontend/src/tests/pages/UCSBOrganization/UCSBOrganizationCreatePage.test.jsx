ce89fe8a10fabe4f7d50b78f19fc06782a74c8c5
Pau Marsa <paumarsa@ucsb.edu>
2026-05-05 17:57:06 -0700

    fix merge issues

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
@@ -1,126 +1,45 @@
-import { render, waitFor, fireEvent, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import UCSBOrganizationCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MemoryRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import { expect } from "vitest";
-
-const mockToast = vi.fn();
-vi.mock("react-toastify", async (importOriginal) => {
-  const originalModule = await importOriginal();
-  return {
-    ...originalModule,
-    toast: vi.fn((x) => mockToast(x)),
-  };
-});
-
-const mockNavigate = vi.fn();
-vi.mock("react-router", async (importOriginal) => {
-  const originalModule = await importOriginal();
-  return {
-    ...originalModule,
-    Navigate: vi.fn((x) => {
-      mockNavigate(x);
-      return null;
-    }),
-  };
-});
 
 describe("UCSBOrganizationCreatePage tests", () => {
-  const axiosMock = new AxiosMockAdapter(axios);
+    const axiosMock = new AxiosMockAdapter(axios);
 
-  beforeEach(() => {
-    axiosMock.reset();
-    axiosMock.resetHistory();
-    axiosMock
-      .onGet("/api/currentUser")
-      .reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock
-      .onGet("/api/systemInfo")
-      .reply(200, systemInfoFixtures.showingNeither);
-  });
-
-  test("renders without crashing", async () => {
-    const queryClient = new QueryClient();
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <UCSBOrganizationCreatePage />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
-
-    await waitFor(() => {
-      expect(
-        screen.getByTestId("UCSBOrganizationForm-orgCode"),
-      ).toBeInTheDocument();
-    });
-  });
-
-  test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
-    const queryClient = new QueryClient();
-    const ucsbOrganization = {
-      orgCode: "ZBT",
-      orgTranslationShort: "Zeta Beta Tau",
-      orgTranslation: "Zeta Beta Tau at UCSB",
-      inactive: "false",
+    const setupUserOnly = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock
+            .onGet("/api/currentUser")
+            .reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock
+            .onGet("/api/systemInfo")
+            .reply(200, systemInfoFixtures.showingNeither);
     };
 
-    axiosMock.onPost("/api/ucsborganization/post").reply(202, ucsbOrganization);
+    const queryClient = new QueryClient();
+    test("Renders expected content", async () => {
+        // arrange
+        setupUserOnly();
 
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <UCSBOrganizationCreatePage />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
+        // act
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <UCSBOrganizationCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>,
+        );
 
-    await waitFor(() => {
-      expect(
-        screen.getByTestId("UCSBOrganizationForm-orgCode"),
-      ).toBeInTheDocument();
+        // assert
+        await screen.findByText("Create page not yet implemented");
+        expect(
+            screen.getByText("Create page not yet implemented"),
+        ).toBeInTheDocument();
     });
-
-    const orgCodeField = screen.getByTestId("UCSBOrganizationForm-orgCode");
-    const orgTranslationShortField = screen.getByTestId(
-      "UCSBOrganizationForm-orgTranslationShort",
-    );
-    const orgTranslationField = screen.getByTestId(
-      "UCSBOrganizationForm-orgTranslation",
-    );
-    const inactiveField = screen.getByTestId("UCSBOrganizationForm-inactive");
-    const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
-
-    fireEvent.change(orgCodeField, { target: { value: "ZBT" } });
-    fireEvent.change(orgTranslationShortField, {
-      target: { value: "Zeta Beta Tau" },
-    });
-    fireEvent.change(orgTranslationField, {
-      target: { value: "Zeta Beta Tau at UCSB" },
-    });
-    fireEvent.change(inactiveField, { target: { value: "false" } });
-
-    expect(submitButton).toBeInTheDocument();
-
-    fireEvent.click(submitButton);
-
-    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
-
-    expect(axiosMock.history.post[0].params).toEqual({
-      orgCode: "ZBT",
-      orgTranslationShort: "Zeta Beta Tau",
-      orgTranslation: "Zeta Beta Tau at UCSB",
-      inactive: "false",
-    });
-
-    expect(mockToast).toBeCalledWith(
-      "New UCSBOrganization Created - orgCode: ZBT orgTranslationShort: Zeta Beta Tau",
-    );
-    expect(mockNavigate).toBeCalledWith({ to: "/ucsborganization" });
-  });
 });
