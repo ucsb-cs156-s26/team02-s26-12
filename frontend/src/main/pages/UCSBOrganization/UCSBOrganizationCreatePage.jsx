59b9c6a42e96aca2191a9d5816a2039e9ab5f990
Pau Marsa <paumarsa@ucsb.edu>
2026-05-05 18:06:03 -0700

    fix merge issues2

--- frontend/src/main/pages/UCSBOrganization/UCSBOrganizationCreatePage.jsx
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationCreatePage.jsx
@@ -1,50 +1,11 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import UCSBOrganizationForm from "main/components/UCSBOrganization/UCSBOrganizationForm";
-import { Navigate } from "react-router";
-import { useBackendMutation } from "main/utils/useBackend";
-import { toast } from "react-toastify";
 
-export default function UCSBOrganizationCreatePage({ storybook = false }) {
-  const objectToAxiosParams = (ucsborganization) => ({
-    url: "/api/ucsborganization/post",
-    method: "POST",
-    params: {
-      orgCode: ucsborganization.orgCode,
-      orgTranslationShort: ucsborganization.orgTranslationShort,
-      orgTranslation: ucsborganization.orgTranslation,
-      inactive: ucsborganization.inactive,
-    },
-  });
-
-  const onSuccess = (ucsborganization) => {
-    toast(
-      `New UCSBOrganization Created - orgCode: ${ucsborganization.orgCode} orgTranslationShort: ${ucsborganization.orgTranslationShort}`,
-    );
-  };
-
-  const mutation = useBackendMutation(
-    objectToAxiosParams,
-    { onSuccess },
-    // Stryker disable next-line all : hard to set up test for caching
-    ["/api/ucsborganization/all"],
-  );
-
-  const { isSuccess } = mutation;
-
-  const onSubmit = async (data) => {
-    mutation.mutate(data);
-  };
-
-  if (isSuccess && !storybook) {
-    return <Navigate to="/ucsborganization" />;
-  }
-
+export default function UCSBOrganizationCreatePage() {
+  // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create New UCSBOrganization</h1>
-
-        <UCSBOrganizationForm submitAction={onSubmit} />
+        <h1>Create page not yet implemented</h1>
       </div>
     </BasicLayout>
   );
--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationCreatePage.test.jsx
@@ -9,37 +9,37 @@ import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
 describe("UCSBOrganizationCreatePage tests", () => {
-    const axiosMock = new AxiosMockAdapter(axios);
+  const axiosMock = new AxiosMockAdapter(axios);
 
-    const setupUserOnly = () => {
-        axiosMock.reset();
-        axiosMock.resetHistory();
-        axiosMock
-            .onGet("/api/currentUser")
-            .reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock
-            .onGet("/api/systemInfo")
-            .reply(200, systemInfoFixtures.showingNeither);
-    };
+  const setupUserOnly = () => {
+    axiosMock.reset();
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  };
 
-    const queryClient = new QueryClient();
-    test("Renders expected content", async () => {
-        // arrange
-        setupUserOnly();
+  const queryClient = new QueryClient();
+  test("Renders expected content", async () => {
+    // arrange
+    setupUserOnly();
 
-        // act
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <UCSBOrganizationCreatePage />
-                </MemoryRouter>
-            </QueryClientProvider>,
-        );
+    // act
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <UCSBOrganizationCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
 
-        // assert
-        await screen.findByText("Create page not yet implemented");
-        expect(
-            screen.getByText("Create page not yet implemented"),
-        ).toBeInTheDocument();
-    });
+    // assert
+    await screen.findByText("Create page not yet implemented");
+    expect(
+      screen.getByText("Create page not yet implemented"),
+    ).toBeInTheDocument();
+  });
 });
